f670ac28683af311c64945f20c6b5d8da450a923
Mario Celi <mceli@stackbuilders.com>
2015-12-03 10:21:07 -0500

    Reorganize code, ChannelManager and IPMessagingManager

--- twiliochat/ChannelManager.m
+++ twiliochat/ChannelManager.m
@@ -25,16 +25,7 @@ NSString *defaultChannelName = @"General Channel";
     return self;
 }
 
-- (void)createGeneralChatRoomWithBlock:(void(^)(BOOL succeeded))block {
-    [self.channelsList createChannelWithFriendlyName:defaultChannelName
-                                                type:TWMChannelTypePublic
-                                          completion:^(TWMResult result, TWMChannel *channel) {
-                                              if (result == TWMResultSuccess) {
-                                                  self.generalChatroom = channel;
-                                              }
-                                              if (block) block(result == TWMResultSuccess);
-                                          }];
-}
+#pragma mark General channel
 
 - (void)joinGeneralChatRoomWithBlock:(void(^)(BOOL succeeded))block {
     [self populateChannelsWithBlock:^(BOOL succeeded) {
@@ -66,24 +57,24 @@ NSString *defaultChannelName = @"General Channel";
     }];
 }
 
+- (void)createGeneralChatRoomWithBlock:(void(^)(BOOL succeeded))block {
+    [self.channelsList createChannelWithFriendlyName:defaultChannelName
+                                                type:TWMChannelTypePublic
+                                          completion:^(TWMResult result, TWMChannel *channel) {
+                                              if (result == TWMResultSuccess) {
+                                                  self.generalChatroom = channel;
+                                              }
+                                              if (block) block(result == TWMResultSuccess);
+                                          }];
+}
+
 - (void)setGeneralChatRoomUniqueNameWithBlock:(void(^)(BOOL succeeded))block {
     [self.generalChatroom setUniqueName:defaultChannelUniqueName completion:^(TWMResult result) {
         if (block) block(result == TWMResultSuccess);
     }];
 }
 
-- (void)loadChannelListWithBlock:(void(^)(TWMResult result, TWMChannels *channelsList))block {
-    self.channelsList = nil;
-    
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        [[IPMessagingManager sharedManager].client channelsListWithCompletion:^(TWMResult result, TWMChannels *channelsList) {
-            if (result == TWMResultSuccess) {
-                self.channelsList = channelsList;
-            }
-            if (block) block(result, self.channelsList);
-        }];
-    });
-}
+#pragma mark Populate channels
 
 - (void)populateChannelsWithBlock:(void(^)(BOOL succeeded))block {
     self.channels = nil;
@@ -95,7 +86,7 @@ NSString *defaultChannelName = @"General Channel";
                     if (result == TWMResultSuccess) {
                         self.channels = [[NSMutableOrderedSet alloc] init];
                         [self.channels addObjectsFromArray:[self.channelsList allObjects]];
-                        [self sortChannels];                        
+                        [self sortChannels];
                     }
                     dispatch_async(dispatch_get_main_queue(), ^{
                         if (block) block(result);
@@ -113,6 +104,19 @@ NSString *defaultChannelName = @"General Channel";
     }];
 }
 
+- (void)loadChannelListWithBlock:(void(^)(TWMResult result, TWMChannels *channelsList))block {
+    self.channelsList = nil;
+
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [[IPMessagingManager sharedManager].client channelsListWithCompletion:^(TWMResult result, TWMChannels *channelsList) {
+            if (result == TWMResultSuccess) {
+                self.channelsList = channelsList;
+            }
+            if (block) block(result, self.channelsList);
+        }];
+    });
+}
+
 - (void)sortChannels {
     SEL sortSelector = @selector(localizedCaseInsensitiveCompare:);
     NSSortDescriptor *descriptor = [[NSSortDescriptor alloc] initWithKey:@"friendlyName"
@@ -120,6 +124,8 @@ NSString *defaultChannelName = @"General Channel";
                                                                 selector:sortSelector];
     [self.channels sortUsingDescriptors:@[descriptor]];
 }
+
+# pragma mark Create channel
 
 - (void)createChannelWithName:(NSString *)name block:(void(^)(BOOL succeeded, TWMChannel *channel))block {
     if ([name isEqualToString:defaultChannelName]) {
@@ -146,6 +152,8 @@ NSString *defaultChannelName = @"General Channel";
                                               if (block) block(result == TWMResultSuccess, channel);
                                           }];
 }
+
+# pragma mark TwilioIPMessagingClientDelegate
 
 - (void)ipMessagingClient:(TwilioIPMessagingClient *)client channelAdded:(TWMChannel *)channel {
     dispatch_async(dispatch_get_main_queue(), ^{
